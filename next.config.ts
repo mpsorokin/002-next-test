e325070b856cc03a389f8dc57a618f25d24f0fa9
Aa <aa@example.com>
2025-03-13 16:27:23 +0200

    working state

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: 'export',
-    basePath: '/002-next-test',
-    assetPrefix: '/002-next-test'
 };
 
 export default nextConfig;
